33689f044a949240cebf59eac81b81155a6d4230
maty139cz <83394404+maty139cz@users.noreply.github.com>
2021-11-11 15:11:44 +0100

    vložení základního souboru

---
+++ setcal.c
@@ -0,0 +1,18 @@
+//TODO header 
+
+// --Util functions--
+
+// --File processing--
+
+// --Set functions--
+
+// --Relation functions--
+
+// --Print functions-- 
+
+
+int main(int argc, char const *argv[])
+{
+    //TODO
+    return 0;
+}
